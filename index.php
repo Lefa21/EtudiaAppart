9456141a172b3cd53793ecff997f15e8a7aafc60
Tounzitoun <faelben@outlook.fr>
2024-12-08 21:44:23 +0100

    ADD page Inscription/Connexion

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'connexion.php';
-require_once 'vue_generique.php';
+require_once './src/connexion.php';
+require_once './src/vue_generique.php';
 
 session_start();
 
@@ -8,18 +8,22 @@ Connexion::initConnexion();
 
 // permet de récuperer le module choisi par l'utilisateur
 $module = $_GET['module'] ?? 'home';
-
 $moduleClass = '';
 $moduleFile = '';
 
 switch ($module) {
     case 'home':
-        $moduleFile = './modules/home/module_home.php';
+        $moduleFile = './src/modules/home/module_home.php';
         $moduleClass = 'ModuleHome';
         break;
     case 'connexion':
-        $moduleFile = './modules/connexion/module_connexion.php';
+        $moduleFile = './src/modules/connexion/module_connexion.php';
         $moduleClass = 'ModConnexion';
+        break;
+
+    case 'inscription':
+        $moduleFile = './src/modules/inscription/module_inscription.php';
+        $moduleClass = 'ModInscription';
         break;
 }
 
@@ -34,4 +38,4 @@ if (file_exists($moduleFile)) {
 
 $tampon = $moduleClass->displayContent();
 
-include_once 'template.php';
+include_once './src/template.php';
--- src/modules/connexion/modele_connexion.php
+++ src/modules/connexion/modele_connexion.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'connexion.php';
+require_once __DIR__  . '/../../connexion.php';
 
 class ModeleConnexion extends Connexion
 {
@@ -9,49 +9,24 @@ class ModeleConnexion extends Connexion
     }
 
 
-    public function ajoutUtilisateur()
-    {
-        // Vérifier si le formulaire est soumis 
-        if (isset($_POST['submit']) && isset($_POST['login']) && isset($_POST['password'])) {
-            $login = $_POST['login'];
-            $sql = Connexion::getBdd()->prepare('SELECT * FROM joueur WHERE username = :login');
-            $sql->bindParam(':login', $login, PDO::PARAM_STR); // Assurez-vous de lier le paramètre :login avec une valeur $login
-            $sql->execute();
-            if ($sql->rowCount() >= 1) {
-                echo 'Ce login existe déjà' . '</br>';
-                return;
-            } else {
-                $hashMotDePasse = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                $password = $hashMotDePasse;
-                $sql = Connexion::getBdd()->prepare('INSERT INTO joueur (username,password,money,img_profil,online) VALUES (:login,:password,0,null,true)');
-                $sql->bindParam(':login', $login);
-                $sql->bindParam(':password', $password);
-                // insert one row
-                $sql->execute();
-                echo 'Inscription validé' . '</br>';
-            }
-        }
-    }
-
-
     public function connexionUtilisateur()
     {
-        if (isset($_POST['submit']) && isset($_POST['login']) && isset($_POST['password'])) {
-            if (isset($_SESSION['identifiant_utilisateur']) && $_SESSION['identifiant_utilisateur'] == $_POST['login']) {
+        if (isset($_POST['submit']) && isset($_POST['email']) && isset($_POST['password'])) {
+            if (isset($_SESSION['identifiant_utilisateur']) && $_SESSION['identifiant_utilisateur'] == $_POST['email']) {
                 echo 'Vous êtes déjà connecté sous l\'identifiant : ' . $_SESSION['identifiant_utilisateur'] . '<br>';
                 return;
             } else {
-                $login = $_POST['login'];
+                $email = $_POST['email'];
                 $password = $_POST['password'];
 
-                $sql = Connexion::getBdd()->prepare('SELECT * FROM joueur WHERE username = :login');
-                $sql->bindParam(':login', $login, PDO::PARAM_STR);
+                $sql = Connexion::getBdd()->prepare('SELECT * FROM User WHERE email = :email');
+                $sql->bindParam(':email', $email, PDO::PARAM_STR);
                 $sql->execute();
 
                 if ($sql->rowCount() == 1) {
                     $row = $sql->fetch(PDO::FETCH_ASSOC); // permet de récupérer les données de la requete
                     if (password_verify($password, $row['password'])) {
-                        $_SESSION['identifiant_utilisateur'] = $login;
+                        $_SESSION['identifiant_utilisateur'] = $email;
                         header('Location: index.php');
 //              echo 'variable session : ' . $_SESSION['identifiant_utilisateur'] .'<br>';
 //              echo 'Connexion validée' . '</br>';
