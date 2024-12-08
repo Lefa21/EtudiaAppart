44a8d1c8be45142a424f4e6f6f11f4d6f9e03ed2
Tounzitoun <faelben@outlook.fr>
2024-12-08 16:45:29 +0100

    ADD agencement architecture projet

--- src/template.php
+++ src/template.php
@@ -1,37 +1,35 @@
-<?php
-require_once 'connexion.php';
-require_once 'vue_generique.php';
+<?php global $tampon ?>
+<!DOCTYPE html >
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EtudiAppart</title>
+    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="./src/css/utilities.css">
+</head>
+<body>
+    <header class="entete bg-color">
+        <nav class="navigation-bar">
+            <div class="container-logo"> 
+                <a  href="index.php"><img src="./assets/logo.png" alt="Logo du site web"></a>
+                <a class="etudiappart-title" href="">EtudiAppart</a> 
+            </div>
+            <ul>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Déposer une annonce</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a  class="right" href="#">Inscription</a></li>
+            </ul>
+        </nav>
+    </header>
 
-session_start();
+    <main>
+        <?php echo $tampon; ?>
+    </main>
 
-Connexion::initConnexion();
-
-// permet de récuperer le module choisi par l'utilisateur
-$module = $_GET['module'] ?? 'home';
-
-$moduleClass = '';
-$moduleFile = '';
-
-switch ($module) {
-    case 'home':
-        $moduleFile = './modules/home/module_home.php';
-        $moduleClass = 'ModuleHome';
-        break;
-    case 'connexion':
-        $moduleFile = './modules/connexion/module_connexion.php';
-        $moduleClass = 'ModConnexion';
-        break;
-}
-
-if (file_exists($moduleFile)) {
-    require $moduleFile;
-    $moduleClass = new $moduleClass();
-} else {
-    var_dump($moduleFile);
-    var_dump($moduleClass);
-    echo "Erreur sur le module dans l'index";
-}
-
-$tampon = $moduleClass->displayContent();
-
-include_once 'template.php';
+    <footer>
+        <p>&copy; 2024 EtudiAppart. All rights reserved.</p>
+    </footer>
+</body>
+</html>
