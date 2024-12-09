3e9f1a6455234ee44e7be8dc675aabb72e271fa5
antoinebnm <ab20040218@gmail.com>
2024-12-09 09:50:06 +0100

    packages node

--- src/template.php
+++ src/template.php
@@ -1,6 +1,7 @@
-<?php global $tampon ?>
-<!DOCTYPE html >
+<?php global $tampon; ?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,12 +10,13 @@
     <link rel="stylesheet" href="./src/css/style.css">
     <link rel="stylesheet" href="./src/css/utilities.css">
 </head>
+
 <body>
     <header class="entete bg-color-navbar">
         <nav class="navigation-bar">
-            <div class="container-logo"> 
-                <a  href="index.php"><img src="./assets/logo.png" alt="Logo du site web"></a>
-                <a class="etudiappart-title" href="">EtudiAppart</a> 
+            <div class="container-logo">
+                <a href="index.php"><img src="./assets/logo.png" alt="Logo du site web"></a>
+                <a class="etudiappart-title" href="">EtudiAppart</a>
             </div>
             <ul>
                 <li><a href="">A propos</a></li>
@@ -24,7 +26,7 @@
             </ul>
         </nav>
     </header>
-    
+
     <main>
         <?php echo $tampon; ?>
     </main>
@@ -34,4 +36,5 @@
     </footer>
 
 </body>
+
 </html>
