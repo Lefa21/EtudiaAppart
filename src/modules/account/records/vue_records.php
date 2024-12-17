60b9992fce3f41f8f18d37a98a0a068f4d105488
antoinebnm <ab20040218@gmail.com>
2024-12-17 01:54:56 +0100

    records static info section done

--- src/modules/account/records/vue_records.php
+++ src/modules/account/records/vue_records.php
@@ -22,15 +22,15 @@ class VueRecords extends VueGenerique
                 </div>
 
                 <nav class="nav-menu" role="navigation" aria-label="Menu principal">
-                    <a href="#profile" class="nav-item">
+                    <a href="index.php?module=monProfil" class="nav-item">
                         <img src="assets/icon_profil.svg" alt="" class="nav-icon" width="30" height="30" aria-hidden="true" />
                         <span>Profil</span>
                     </a>
-                    <a href="#dossier" class="nav-item">
+                    <a href="index.php?module=records" class="nav-item active">
                         <img src="assets/icon_documents_profile.svg" alt="" class="nav-icon" width="30" height="30" aria-hidden="true" />
                         <span>Mon dossier</span>
                     </a>
-                    <a href="#requests" class="nav-item active" aria-current="page">
+                    <a href="#requests" class="nav-item" aria-current="page">
                         <img src="assets/icon_follow_request.svg" alt="" class="nav-icon" width="30" height="30" aria-hidden="true" />
                         <span>Suivi des demandes</span>
                     </a>
@@ -71,6 +71,7 @@ class VueRecords extends VueGenerique
                         <section id="dropfile-scholarship_proof" class="dropfile_section">
                             <div class="dropfile_info">
                                 <span>Attestation de bourse</span><span class="more_info">&#9432;</span>
+                                <p>* Étudiants boursiers seulement</p>
                             </div>
                             <div class="dropfile_file">
                                 <input type="file" id="scholarship_proof-file" accept=".pdf" />
@@ -79,7 +80,7 @@ class VueRecords extends VueGenerique
                         <section id="dropfile-visa" class="dropfile_section">
                             <div class="dropfile_info">
                                 <span>Visa / Titre de séjour</span><span class="more_info">&#9432;</span>
-                                <p style="font-size: small;">* Étudiants internationnaux seulement</p>
+                                <p>* Étudiants internationnaux seulement</p>
                             </div>
                             <div class="dropfile_file">
                                 <input type="file" id="visa-file" accept=".pdf" />
@@ -101,6 +102,44 @@ class VueRecords extends VueGenerique
                         <span class="menu_button_text">Informations complémentaires</span><img class="menu_button_arrow" src="./assets/black_arrow-right.svg" alt="" />
                     </div>
                     <div class="dropdown-menu document_section">
+                        <form>
+                            <section class="info_section">
+                                <span class="info_section_text">Êtes-vous étudiant boursier ?</span>
+                                <div class="info_section_zone">
+                                    <input type="radio" id="scholarship-yes" name="scholarship" value="yes" />
+                                    <label for="scholarship-yes">Oui</label>
+                                    <input type="radio" id="scholarship-no" name="scholarship" value="no" />
+                                    <label for="scholarship-no">Non</label>
+                                </div>
+                            </section>
+                            <section class="info_section">
+                                <span class="info_section_text">Quel est votre status ?</span>
+                                <div class="info_section_zone">
+                                    <input type="radio" id="status-owner" name="status" value="owner" />
+                                    <label for="status-owner">Propriétaire</label>
+                                    <input type="radio" id="status-tenant" name="status" value="tenant" />
+                                    <label for="status-tenant">Locataire</label>
+                                </div>
+                            </section>
+                            <section class="info_section">
+                                <span class="info_section_text">Date de naissance</span>
+                                <div class="info_section_zone">
+                                    <input type="date" id="birthdate" name="birthdate" value="birthdate" />
+                                </div>
+                            </section>
+                            <section class="info_section">
+                                <span class="info_section_text">Nationnalité</span>
+                                <div class="info_section_zone">
+                                    <input type="text" id="nationality" name="nationality" placeholder="Nationality" size="16" />
+                                </div>
+                            </section>
+                            <section class="info_section">
+                                <span class="info_section_text">Présentation</span>
+                                <div class="info_section_zone relative">
+                                    <textarea name="presentation" rows="5" cols="30"></textarea>
+                                </div>
+                            </section>
+                        </form>
                         <button id="save_docs-more_information" class="action-button" type="submit">Enregistrer les modifications</button>
                     </div>
                 </div>
