052e0c57ecde60ba8eaef5dcf299fc97b71d064d
Tristannnnnnnnnnnn <tristan.passedat.isep@gmail.com>
2025-01-05 17:34:24 +0100

    dynamique qui me fait cabler

--- src/modules/creation_annonce/modele_creation_annonce.php
+++ src/modules/creation_annonce/modele_creation_annonce.php
@@ -9,46 +9,49 @@ class ModeleCreationAnnonce extends Connexion
     }
     public function ajoutInfos()
     {
-        if(isset($_POST['meuble']) && isset($_POST['titre_form']) && isset($_POST['type_logement_form']) && isset($_POST['prix_form']) && isset($_POST['superficie_form']) && isset($_POST['nb_pieces_form']) && isset($_POST['debut_form']) && isset($_POST['fin_form']) && isset($_POST['loc_form']) && isset($_POST['ville_form']) && isset($_POST['cp_form']) && isset($_POST['region_form'])) {
-            $titre_form = $_POST['titre_form'];
-            $type_logement_form = $_POST['type_logement_form'];
-            $prix_form = $_POST['prix_form'];
-            $meuble = $_POST['meuble'];
-            $superficie_form = $_POST['superficie_form'];
-            $nb_pieces_form = $_POST['nb_pieces_form'];
-            $debut_form = $_POST['debut_form'];
-            $fin_form = $_POST['fin_form'];
+//        && isset($_POST['meuble']) && isset($_POST['titre_form']) && isset($_POST['type_logement_form']) && isset($_POST['prix_form']) && isset($_POST['superficie_form']) && isset($_POST['nb_pieces_form']) && isset($_POST['debut_form']) && isset($_POST['fin_form'])
+        if(isset($_POST['sub_info']) &&  isset($_POST['loc_form']) && isset($_POST['ville_form']) && isset($_POST['cp_form']) && isset($_POST['region_form'])) {
+//            $titre_form = $_POST['titre_form'];
+//            $type_logement_form = $_POST['type_logement_form'];
+//            $prix_form = $_POST['prix_form'];
+//            $meuble = $_POST['meuble'];
+//            $superficie_form = $_POST['superficie_form'];
+//            $nb_pieces_form = $_POST['nb_pieces_form'];
+//            $debut_form = $_POST['debut_form'];
+//            $fin_form = $_POST['fin_form'];
             $loc_form = $_POST['loc_form'];
             $ville_form = $_POST['ville_form'];
             $cp_form = $_POST['cp_form'];
             $region_form = $_POST['region_form'];
-            Connexion::getBdd()->beginTransaction();
-            $sql = Connexion::getBdd()->prepare('INSERT INTO Ad (rent_price, lease_start, lease_end, ad_title) VALUES (:prix_form, :debut_form, :fin_form, :titre_form)');
-            $sql->bindParam(':prix_form', $prix_form);
-            $sql->bindParam(':debut_form', $debut_form);
-            $sql->bindParam(':fin_form', $fin_form);
-            $sql->bindParam(':titre_form', $titre_form);
-            $sql->execute();
-            Connexion::getBdd()->beginTransaction();
-            $sql = Connexion::getBdd()->prepare('INSERT INTO Habitation (numbers_rooms, furnished, type_habitation, surface_area) VALUES (:nb_pieces_form, :meuble, :type_logement_form, :superficie_form)');
-            $sql->bindParam(':nb_pieces_form', $nb_pieces_form);
-            $sql->bindParam(':meuble', $meuble);
-            $sql->bindParam(':type_logement_form', $type_logement_form);
-            $sql->bindParam(':superficie_form', $superficie_form);
-            $sql->execute();
-            Connexion::getBdd()->beginTransaction();
-            $sql = Connexion::getBdd()->prepare('INSERT INTO Address (address_line, city, zipcode, country) VALUES (:loc_form, :ville_form, :cp_form, :region_form)');
-            $sql->bindParam(':loc_form', $loc_form);
-            $sql->bindParam(':ville_form', $ville_form);
-            $sql->bindParam(':cp_form', $cp_form);
-            $sql->bindParam(':region_form', $region_form);
-            $sql->execute();
-            header('Location: index.php?module=creation_annonce&action=ajout_infos');
+//            $sql1 = Connexion::getBdd()->prepare('INSERT INTO Ad (date_publication, rent_price, description, lease_start, lease_end, ad_title) VALUES (NOW(), :prix_form, :description, :debut_form, :fin_form, :titre_form)');
+//            $sql1->bindParam(':prix_form', $prix_form);
+//            $sql1->bindParam(':debut_form', $debut_form);
+//            $sql1->bindParam(':fin_form', $fin_form);
+//            $sql1->bindParam(':titre_form', $titre_form);
+//            $sql1->bindParam(':description', '');
+//            $sql2 = Connexion::getBdd()->prepare('INSERT INTO Habitation (numbers_rooms, furnished, type_habitation, surface_area) VALUES (:nb_pieces_form, :meuble, :type_logement_form, :superficie_form)');
+//            $sql2->bindParam(':nb_pieces_form', $nb_pieces_form);
+//            $sql2->bindParam(':meuble', $meuble);
+//            $sql2->bindParam(':type_logement_form', $type_logement_form);
+//            $sql2->bindParam(':superficie_form', $superficie_form);
+            $sql3 = Connexion::getBdd()->prepare('INSERT INTO Address (address_line, city, zipcode, country) VALUES (:loc_form, :ville_form, :cp_form, :region_form)');
+            $sql3->bindParam(':loc_form', $loc_form);
+            $sql3->bindParam(':ville_form', $ville_form);
+            $sql3->bindParam(':cp_form', $cp_form);
+            $sql3->bindParam(':region_form', $region_form);
+            if ($sql3->execute()) {
+                header('index.php?module=creation_annonce&action=formulaireCreationAnnonce');
+//                echo 'l\'annonce a bien été prise en compte' . '</br>';
+            } else {
+                echo 'Erreur lors de l\'inscription, veuillez réessayer plus tard.' . '</br>';
+            }
+
 
         }
-        else {
-            echo '<script type="text/javascript">window.onload = function () { alert("Tous les champs doivent être remplis."); } </script>';
-        }
+//        else { $sql1->execute() && $sql2->execute() &&
+//            header('index.php?module=creation_annonce&action=formulaireCreationAnnonce');
+//            echo '<script type="text/javascript">window.onload = function () { alert("Tous les champs doivent être remplis."); } </script>';
+//        }
 
     }
     public function ajoutPhotos(){
