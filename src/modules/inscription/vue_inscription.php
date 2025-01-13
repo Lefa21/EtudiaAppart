7b3d2ae64bd177b24b6306b2c780c17ae3f1dc15
Amiiine27 <amineakh213@outlook.fr>
2025-01-13 18:35:50 +0100

    Ajout du champ 'role' dans lors de l'inscription, back et front OK

--- src/modules/inscription/vue_inscription.php
+++ src/modules/inscription/vue_inscription.php
@@ -4,90 +4,103 @@ require_once __DIR__  . '/../../vue_generique.php';
 
 class VueInscription extends VueGenerique
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-    public function formulaireInscription()
-    {
+  public function formulaireInscription()
+  {
 ?>
-<link rel="stylesheet" href="./src/css/inscription.css">
-  <div class="registration-container">
-    <main class="main-content-inscription">
-      <div class="registration-wrapper-inscription">
-        <form class="registration-form-inscription" action="index.php?module=inscription&action=inscription" method="POST">
-          <h1 class="form-title-inscription">Inscription</h1>
-          
-          <label for="lastname" class="visually-hidden">Nom</label>
-          <input 
-            type="text" 
-            id="lastname" 
-            name="last_name"
-            class="form-input-inscription" 
-            placeholder="Nom" 
-            required 
-            aria-required="true"
-          />
-          
-          <label for="firstname" class="visually-hidden">Prénom</label>
-          <input 
-            type="text" 
-            id="firstname" 
-            name="first_name"
-            class="form-input-inscription" 
-            placeholder="Prénom" 
-            required
-            aria-required="true"
-          />
-          
-          <label for="email" class="visually-hidden">Adresse e-mail</label>
-          <input 
-            type="email" 
-            id="email" 
-            name="email"
-            class="form-input-inscription" 
-            placeholder="Adresse e-mail" 
-            required
-            aria-required="true"
-          />
-          
-          <label for="password" class="visually-hidden">Mot de passe</label>
-          <input 
-            type="password" 
-            id="password" 
-            name="password"
-            class="form-input-inscription" 
-            placeholder="Mot de passe" 
-            required
-            aria-required="true"
-          />
-          
-          <label for="confirm-password" class="visually-hidden">Confirmer votre mot de passe</label>
-          <input 
-            type="password" 
-            id="confirm-password" 
-            name="confirm_password"
-            class="form-input-inscription" 
-            placeholder="Confirmer votre mot de passe" 
-            required
-            aria-required="true"
-          />
-          
-          <button type="submit" name="submit" class="submit-btn-inscription">
-            Sauvegarder
-          </button>
-          
-          <p class="login-link-inscription">
-            Déja de compte ? 
-            <a href="index.php?module=connexion&action=formulaireConnexion" class="login-link-inscription">
-              Connectez-vous
-            </a>
-          </p>
-        </form>
-      </div>
-    </main>
-  </div>
+    <link rel="stylesheet" href="./src/css/inscription.css">
+    <div class="registration-container">
+      <main class="main-content-inscription">
+        <div class="registration-wrapper-inscription">
+          <form class="registration-form-inscription" action="index.php?module=inscription&action=inscription" method="POST">
+            <h1 class="form-title-inscription">Inscription</h1>
+
+            <label for="lastname" class="visually-hidden">Nom</label>
+            <input
+              type="text"
+              id="lastname"
+              name="last_name"
+              class="form-input-inscription"
+              placeholder="Nom"
+              required
+              aria-required="true" />
+
+            <label for="firstname" class="visually-hidden">Prénom</label>
+            <input
+              type="text"
+              id="firstname"
+              name="first_name"
+              class="form-input-inscription"
+              placeholder="Prénom"
+              required
+              aria-required="true" />
+
+            <label for="email" class="visually-hidden">Adresse e-mail</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="form-input-inscription"
+              placeholder="Adresse e-mail"
+              required
+              aria-required="true" />
+
+            <select name="role"
+              id="role"
+              class="role"
+              style="
+                border-radius: 5px;
+                border: 1px solid var(--Primary, #041a8f);
+                background: #fff;
+                padding: 20px 10px;
+                margin-top: 38px;
+                width: 100%;
+                font-size: 20px;
+                color: var(--Black, #000);
+              ">
+              <option value="">Rôle</option>
+              <option value="etudiant">Etudiant</option>
+              <option value="proprietaire">Propriétaire</option>
+            </select>
+
+            <label for="password" class="visually-hidden">Mot de passe</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              class="form-input-inscription"
+              placeholder="Mot de passe"
+              required
+              aria-required="true" />
+
+            <label for="confirm-password" class="visually-hidden">Confirmer votre mot de passe</label>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm_password"
+              class="form-input-inscription"
+              placeholder="Confirmer votre mot de passe"
+              required
+              aria-required="true" />
+
+            <button type="submit" name="submit" class="submit-btn-inscription">
+              Sauvegarder
+            </button>
+
+            <p class="login-link-inscription">
+              Déja de compte ?
+              <a href="index.php?module=connexion&action=formulaireConnexion" class="login-link-inscription">
+                Connectez-vous
+              </a>
+            </p>
+          </form>
+        </div>
+      </main>
+    </div>
 <?php
-    }
+  }
 }
