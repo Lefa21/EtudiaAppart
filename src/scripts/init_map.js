e173a5eb9ace2f12e609bf71fa733a226a80f650
antoinebnm <ab20040218@gmail.com>
2025-01-18 01:58:33 +0100

    clean filter (ad_search) checkbox; fixed google api maps not loading correctly

--- src/scripts/init_map.js
+++ src/scripts/init_map.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   initMap();
 });
 
@@ -6,49 +6,51 @@ function initMap() {
   const defaultLocation = { lat: 48.8566, lng: 2.3522 };
 
   const map = new google.maps.Map(document.getElementById("google-map"), {
-      zoom: 12, 
-      center: defaultLocation,
+    zoom: 12,
+    center: defaultLocation,
   });
 
   const adDataElement = document.getElementById("adData");
   if (!adDataElement) {
-      console.error("L'élément adData n'a pas été trouvé dans le DOM.");
-      return;
+    console.error("L'élément adData n'a pas été trouvé dans le DOM.");
+    return;
   }
 
   const adData = JSON.parse(adDataElement.textContent);
-  console.log(adData); 
+  console.log(adData);
 
   if (!adData || adData.length === 0) {
-      console.error("Aucune donnée à afficher sur la carte.");
-      return;
+    console.error("Aucune donnée à afficher sur la carte.");
+    return;
   }
 
   const geocoder = new google.maps.Geocoder();
 
-  adData.forEach(ad => {
-      const city = ad.city;
-      const zipCode = ad.zipCode;
-      const country = ad.country;
+  adData.forEach((ad) => {
+    const city = ad.city;
+    const zipCode = ad.zipCode;
+    const country = ad.country;
 
-      if (city && zipCode && country) {
-          const address = `${city}, ${zipCode}, ${country}`;
-          console.log(address);
+    if (city && zipCode && country) {
+      const address = `${city}, ${zipCode}, ${country}`;
+      console.log(address);
 
-          geocoder.geocode({ address: address }, (results, status) => {
-              if (status === google.maps.GeocoderStatus.OK) {
-                  const location = results[0].geometry.location;
-                  new google.maps.Marker({
-                      position: location,
-                      map: map,
-                      title: ad.ad_title,
-                  });
-              } else {
-                  console.error(`Erreur de géocodage pour l'adresse : ${address}, statut : ${status}`);
-              }
+      geocoder.geocode({ address: address }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          const location = results[0].geometry.location;
+          new google.maps.Marker({
+            position: location,
+            map: map,
+            title: ad.ad_title,
           });
-      } else {
-          console.warn(`Données manquantes pour l'annonce : ${JSON.stringify(ad)}`);
-      }
+        } else {
+          console.error(
+            `Erreur de géocodage pour l'adresse : ${address}, statut : ${status}`,
+          );
+        }
+      });
+    } else {
+      console.warn(`Données manquantes pour l'annonce : ${JSON.stringify(ad)}`);
+    }
   });
 }
